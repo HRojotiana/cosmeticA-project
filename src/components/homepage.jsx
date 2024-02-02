c56363461937d3605f265919504961df87264b27
MC <mc@MacBook-Pro.local>
2024-02-02 09:31:13 +0300

    initial commit

---
+++ src/components/homepage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Homepage() {
+    return(
+        <div className="bg-gray-100 p-8">
+      <h1 className="text-3xl font-bold text-gray-800">Hello, Tailwind CSS!</h1>
+      <p className="text-gray-600 mt-2">Tailwind CSS is ready to use in your React project.</p>
+    </div>
+    );
+}
+export default Homepage;
